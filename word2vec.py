b292348d36d3d1626cf17ae6a20257c0941b4b0e
Song Yang <songyang1218@gmail.com>
2020-10-21 18:30:30 -0400

    Working corpus class

--- word2vec.py
+++ word2vec.py
@@ -1,49 +1,43 @@
 import logging
 import os
-from sys import stderr, stdin
-from typing import Iterator, List
+from sys import argv, stderr
+from typing import Generator, Iterator, List
 import gensim.models
+from itertools import chain
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 class MyCorpus(object):
     """An interator that yields sentences (lists of str)."""
 
-    def _corpus_stream(self) -> Iterator[str]:
-        """Iterates over lines
-
-        Yields:
-            Iterator[str]: Line in the corpus
-        """
-        if self.from_stdin:
-            for line in stdin:
-                yield line
-        else:
-            for file in os.listdir(self.corpus_directory):
-                for line in open(os.path.join(self.corpus_directory, file)):
-                    yield line
-
-    def __init__(self, from_stdin: bool = True, corpus_directory: str = None) -> None:
-        self.from_stdin = from_stdin if corpus_directory is None else False
+    def __init__(self, corpus_directory: str) -> None:
         self.corpus_directory = corpus_directory
 
     def __iter__(self) -> Iterator[List[str]]:
-        """Iterator implementation
+        corpus = (os.path.join(self.corpus_directory, file) for file in os.listdir(self.corpus_directory))
+        for file in corpus:
+            for line in self.__read_lines(file):
+                yield line.split()
 
-        Yields:
-            List[str]: List of string tokens, preprocessed
-        """
-        for line in self._corpus_stream():
-            # preprocess, if any
-            yield line.split()
+    def __read_lines(self, path: str) -> Iterator[str]:
+        for line in open(path):
+            yield line
 
 def main():
-    corpus = MyCorpus()
+    if len(argv) != 2:
+        print("Requires one argument: path to corpus directory")
+        exit(1)
+    corpus = MyCorpus(argv[1])
     
     # print("Reading from stdin", file=stderr)
     # for line in corpus:
     #     print(line)
     
-    model = gensim.models.Word2Vec(sentences=corpus)
+    model = gensim.models.Word2Vec(sentences=list(corpus))
+
+    for i, word in enumerate(model.wv.vocab):
+        if i == 10:
+            break
+        print(word)
 
 if __name__ == "__main__":
     main()
